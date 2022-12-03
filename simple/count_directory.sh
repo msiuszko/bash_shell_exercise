8e2fd6e056bca4dded0babfc262e8bf756c9fc53
Gosia <gosia@github>
2022-12-03 23:45:29 +0100

    Simple directory counting script

--- simple/count_directory.sh
+++ simple/count_directory.sh
@@ -1,7 +1,5 @@
-
-
-
 #!/bin/bash
+#Prosty skrypt liczący wszystkie katalogi w danej lokalizacji oraz wyświetlający ich nazwy
 
 echo "Total of directories are " && ls -p | grep  / | wc -l
 ls -p | grep  /

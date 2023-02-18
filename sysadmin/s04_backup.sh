34e271725b78736a4e074732b257897dc5249799
Gosia <malgorzata.siuszko@gmail.com>
2023-02-18 11:16:47 +0100

    Script update

--- sysadmin/s04_backup.sh
+++ sysadmin/s04_backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#Wyswietla ile plików i katalogów jest do backup. Wykonuje backup. Wyświetla wynik
 
+#Ten skrypt bash służy do tworzenia kopii zapasowej (backupu) katalogu domowego bieżącego użytkownika
 user=$(whoami)
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
+#Funkcje służą do zliczania plików i katalogów w katalogu źródłowym oraz w skompresowanej kopii zapas
 function total_files {
         find $1 -type f | wc -l
 }
@@ -23,10 +24,13 @@ function total_archived_files {
 
 tar -czf $output $input 2> /dev/null
 
+#Funkcja "total_files" przyjmuje jeden argument - ścieżkę do katalogu - i zwraca liczbę plików w tym katalogu (z wyłączeniem podkatalogów).
 src_files=$( total_files $input )
+#Funkcja "total_directories" przyjmuje jeden argument - ścieżkę do katalogu - i zwraca liczbę katalogów w tym katalogu (z wyłączeniem podkatalogów).
 src_directories=$( total_directories $input )
-
+#Funkcja "total_archived_directories" przyjmuje jeden argument - ścieżkę do pliku skompresowanej kopii zapasowej - i zwraca liczbę katalogów w tej kopii.
 arch_files=$( total_archived_files $output )
+#Funkcja "total_archived_files" przyjmuje jeden argument - ścieżkę do pliku skompresowanej kopii zapasowej - i zwraca liczbę plików w tej kopii.
 arch_directories=$( total_archived_directories $output )
 
 echo "Files to be included: $src_files"
